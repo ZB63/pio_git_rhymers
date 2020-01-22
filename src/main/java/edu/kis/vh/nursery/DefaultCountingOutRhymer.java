07ad8d3c7ee3ae398f86dbf099a885f1f6d4a66e
ZB63 <spaweloszek63@gmail.com>
2020-01-22 17:16:30 +0100

    1.5 - Podpunkt 6 - Dodanie komentarzy JavaDoc

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ *DefaultCountingOutRhymer
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int DEFAULT_VALUE = -1;
@@ -8,31 +11,55 @@ public class DefaultCountingOutRhymer {
     
 	private int[] numbers = new int[NUMBERS_SIZE];
 
+    /**
+     * Metoda getTotal().
+     * @return Indeks ostatnio zapisanego elementu.
+     */
     public int getTotal() {
         return total;
     }
 
     private int total = DEFAULT_VALUE;
 
+    /**
+     * Metoda countIn, sluzy do wprowadzania nowego elementu do tablicy.
+     * @param in
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Metoda callCheck(), sluzy do sprawdzania czy tablica jest pusta.
+     * @return True, jezeli tablica jest pusta.
+     */
     public boolean callCheck() {
         return total == DEFAULT_VALUE;
     }
 
+    /**
+     * Metoda isFull(), sprawdza czy tablica jest pelna.
+     * @return True, jezeli tablica jest pelna.
+     */
     public boolean isFull() {
         return total == 11;
     }
 
+    /**
+     * Metoda zwraca ostatnio dodany element.
+     * @return Ostatnio dodany element, jezeli tablica jest pusta -1.
+     */
     protected int peekaboo() {
         if (callCheck())
             return DEFAULT_VALUE;
         return numbers[total];
     }
 
+    /**
+     * Metoda zwraca ostatnio dodany element i usuwa go.
+     * @return Ostatnio dodany element, jezeli tablica jest pusta -1.
+     */
     public int countOut() {
         if (callCheck())
             return DEFAULT_VALUE;
